290bbeea02e9d4b5177383776c681296d6fc0d50
Muhammad Usama <usamamuhammad715@gmail.com>
2023-12-13 23:17:22 +0100

    main function

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
+// main.go
+
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
-	scrapeBooks("http://books.toscrape.com")
-}
+	url := "http://books.toscrape.com"
 
-func scrapeBooks(url string) {
+	htmlContent, err := fetchPAGE(url)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fetchPage(url)
-	// parseHTML()
-	// extractBookDetails()
+	doc, err := parseHTML(htmlContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	books, err := extractData(doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, book := range books {
+		fmt.Printf("Book: %+v\n", book)
+	}
 }
